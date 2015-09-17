a18b500443820bb841671657aa8cf64342123696
Alan Shaw <alan@tableflip.io>
2015-09-17 10:39:39 +0100

    Upgrade to the latest version of meteor

--- packages/spotify/package.js
+++ packages/spotify/package.js
@@ -1,7 +1,7 @@
 Package.describe({summary: "Control Spotify on Mac OSX with NodeJS and AppleScript."})
 
 // Can't use a semver range here!
-Npm.depends({"spotify-node-applescript": "0.2.1"})
+Npm.depends({"spotify-node-applescript": "0.2.2"})
 
 Package.on_use(function (api) {
   api.export("spotify")
